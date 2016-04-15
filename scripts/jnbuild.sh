f32babe9cb9cfbba0d5ea67eee54acf71e33de7e
William <william.fischer42@gmail.com>
2016-04-15 14:01:35 -0600

    SlightMod
    Changed main source filenames to main.cpp

--- scripts/jnbuild.sh
+++ scripts/jnbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 module load intel
-icpc -std=c++0x -I ~/boost_1_54_0/ -fopenmp ~/IsletSimBatch/source/model/simulator.cpp -o  ~/IsletSimBatch/exe/simulator.exe
-icpc -std=c++0x -I ~/boost_1_54_0/ -fopenmp ~/IsletSimBatch/source/batcher/simBatcher.cpp -o  ~/IsletSimBatch/exe/simBatcher.exe
+icpc -std=c++0x -I ~/boost_1_54_0/ -fopenmp ~/IsletSimBatch/source/model/main.cpp -o  ~/IsletSimBatch/exe/simulator.exe
+icpc -std=c++0x -I ~/boost_1_54_0/ -fopenmp ~/IsletSimBatch/source/batcher/main.cpp -o  ~/IsletSimBatch/exe/simBatcher.exe
